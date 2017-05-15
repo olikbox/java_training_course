4ddfb44c92946b70ffb708d0bfaa3460a98ff34f
olikbox <olikbox@gmail.com>
2017-05-15 13:31:38 +0300

    public class Point added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -5,8 +5,21 @@ public class MyFirstProgram1 {
         System.out.println("Hello!");
     }
 
-}
 
+
+
+
+//double getDistance(Point a) {
+		//return Math.sqrt( Math.pow(x-a.x, 2) + Math.pow(y-a.y, 2) );
+
+}
+//public class Main {
+    //public static void main(String[] args) {
+      //  Point p1 = new Point();
+       // Point p2 = new Point(1,1);
+       // System.out.println("Растстояние между точками "+p1+" и "+p2+" равно "+p1.length(p2));
+   // }
+//}
 
 
 	
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Olga_Verkhovtseva on 5/15/2017.
+ */
+public class Point {
+    public double x; // абсцисса точки
+    public double y; // ордината точки
+}
