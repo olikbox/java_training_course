77b337a89649bb6e8430a0a833b00b0c8a175713
olikbox <olikbox@gmail.com>
2017-05-15 15:43:12 +0300

    Method distance  in class Point added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -7,13 +7,16 @@ public class MyFirstProgram1 {
         Point p1 = new Point(2, 3);
         Point p2 = new Point(4, 5);
 
-        System.out.println("Расстояние между двумя точками равняется" +distance(p1,p2));
+        System.out.println("Расстояние между двумя точками равняется " +p2.distance(p1));
     }
 
+//Moved to Point.java
 
-    public static double distance(Point p1,Point p2) {
-        return Math.sqrt( (p2.x-p1.x)*(p2.x-p1.x)+ (p2.y-p1.y)*(p2.x-p1.x));
-    }
+   // public static double distance(Point p1,Point p2) {
+       // return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.x - p1.x));
+   // }
+
+
 }
 
 
