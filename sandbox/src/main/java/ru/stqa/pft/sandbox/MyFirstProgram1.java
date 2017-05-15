1fe2e04fe5a47e6123c147bd9879f438dcbed8b0
olikbox <olikbox@gmail.com>
2017-05-15 14:53:31 +0300

    public static void main(String[] args) {...} implemented

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -3,23 +3,19 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram1 {
     public static void main(String[] args) {
         System.out.println("Hello!");
+
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(4, 5);
+
+        System.out.println("Расстояние между двумя точками равняется" +distance(p1,p2));
     }
 
 
-
-
-
-//double getDistance(Point a) {
-		//return Math.sqrt( Math.pow(x-a.x, 2) + Math.pow(y-a.y, 2) );
-
+    public static double distance(Point p1,Point p2) {
+        return Math.sqrt( (p2.x-p1.x)*(p2.x-p1.x)+ (p2.y-p1.y)*(p2.x-p1.x));
+    }
 }
-//public class Main {
-    //public static void main(String[] args) {
-      //  Point p1 = new Point();
-       // Point p2 = new Point(1,1);
-       // System.out.println("Растстояние между точками "+p1+" и "+p2+" равно "+p1.length(p2));
-   // }
-//}
+
 
 
 	
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,4 +6,11 @@ package ru.stqa.pft.sandbox;
 public class Point {
     public double x; // абсцисса точки
     public double y; // ордината точки
+
+
+//Конструктор
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 }
