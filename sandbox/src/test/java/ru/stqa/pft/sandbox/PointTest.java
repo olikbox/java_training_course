298165a64d5957a4a017175120574685b657f8be
olikbox <olikbox@gmail.com>
2017-05-16 16:21:23 +0300

    Homework3, Tests have been added

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -7,15 +7,15 @@ import org.testng.annotations.Test;
  * Created by Olga_Verkhovtseva on 5/15/2017.
  */
 public class PointTest {
-    @Test
 
+    @Test //позитивный тест
     public void DistanceTestArea () {
         Point p1 = new Point(2, 3);
         Point p2 = new Point(4, 5);
         Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
     }
 
-    @Test
+    @Test //позитивный тест
     public void DistanceTest () {
         Point p1 = new Point(9, 7);
         Point p2 = new Point(5, 4);
