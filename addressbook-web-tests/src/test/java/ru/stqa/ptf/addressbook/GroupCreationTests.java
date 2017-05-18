f233e31730e3f1d75ca63aa94186a979f094fffc
olikbox <olikbox@gmail.com>
2017-05-18 19:46:13 +0300

    GroupCreationTests refactored

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/GroupCreationTests.java
@@ -18,34 +18,58 @@ public class GroupCreationTests {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
         wd.findElement(By.id("content")).click();
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
-    
+
     @Test
-    public void GroupCreationTests() {
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("Nova Liga");
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("NovaLiga corporation");
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("NovaLiga inc. ");
-        wd.findElement(By.name("submit")).click();
+    public void testGroupCreation() {
+        gotoGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("Nova Liga1", "NovaLiga corporation", "NovaLiga inc. "));
+        submitGroupCretiopn();
+        returntoGroupPage();
+    }
+
+    private void returntoGroupPage() {
         wd.findElement(By.linkText("group page")).click();
     }
-    
+
+    private void submitGroupCretiopn() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    private void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
